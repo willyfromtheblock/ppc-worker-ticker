df04bc5f1507a8a9c3be48e867edd299a1927735
Willy <willy@coinerella.com>
2021-08-13 15:44:35 +0200

    prettier

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-addEventListener('fetch', event => {
+addEventListener('fetch', (event) => {
   event.respondWith(handleRequest())
 })
 
 async function handleRequest() {
   const ppc_data = {
-    PPC: parseFloat(await peercoin_kv.get("PPC_USD"))
+    PPC: parseFloat(await peercoin_kv.get('PPC_USD')),
   }
-  const fiat_data  = await peercoin_kv.get("FIAT_USD",  {type: "json"});
-  const data = Object.assign(ppc_data, fiat_data);
+  const fiat_data = await peercoin_kv.get('FIAT_USD', { type: 'json' })
+  const data = Object.assign(ppc_data, fiat_data)
   const json = JSON.stringify(data, null, 2)
 
   return new Response(json, {
