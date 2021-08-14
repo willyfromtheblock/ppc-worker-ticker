6ea67e3e80badefe45d6261e3f60426d35b181fc
Willy <willy@coinerella.com>
2021-08-14 15:41:58 +0200

    kv key changes

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ addEventListener('fetch', (event) => {
 })
 
 async function handleRequest() {
-  const ppc_data = {
-    PPC: parseFloat(await peercoin_kv.get('PPC_USD')),
-  }
-  const fiat_data = await peercoin_kv.get('FIAT_USD', { type: 'json' })
-  const data = Object.assign(ppc_data, fiat_data)
+  const data = await peercoin_kv.get('prices', { type: 'json' })
   const json = JSON.stringify(data, null, 2)
 
   return new Response(json, {
