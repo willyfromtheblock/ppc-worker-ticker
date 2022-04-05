e09321c9a229f1a23cab104980b16a7cb2576122
Willy <willy@coinerella.com>
2022-04-05 18:58:05 +0200

    add control-origin header

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ addEventListener('fetch', (event) => {
   event.respondWith(handleRequest())
 })
 
-async function handleRequest() {
+async function handleRequest(request) {
   const data = await peercoin_kv.get('prices', { type: 'json' })
   const json = JSON.stringify(data, null, 2)
 
   return new Response(json, {
-    headers: { 'content-type': 'application/json' },
+    headers: { 'content-type': 'application/json', 'Access-Control-Allow-Origin': '*' },
   })
 }
